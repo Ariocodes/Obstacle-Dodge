bc630960c30c0879b9b41671ec5831455fbae900
Ario <Ariobashiri@gmail.com>
2026-09-09 02:39:41 +0300

    object color change OnCollisionEnter

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -5,5 +5,7 @@ public class ObjectHit : MonoBehaviour
     private void OnCollisionEnter(Collision other)
     {
         Debug.Log("Something hit me!");
+        GetComponent<MeshRenderer>().material.color = Color.red;
+        
     }
 }
