2b2acb638b55018c5bccb17dc4675cf04fcd8d3e
Ario <Ariobashiri@gmail.com>
2026-09-09 15:13:28 +0300

    Prefabing is lowkey really useful

--- Assets/Scripts/TriggerProjectile.cs
+++ Assets/Scripts/TriggerProjectile.cs
@@ -3,12 +3,27 @@ using UnityEngine;
 public class TriggerProjectile : MonoBehaviour
 {
     [SerializeField] 
-    GameObject projectiles;
+    GameObject projectile1;
+    [SerializeField] 
+    GameObject projectile2;
+    [SerializeField] 
+    GameObject projectile3;
+    [SerializeField] 
+    GameObject projectile4;
+    [SerializeField] 
+    GameObject projectile5;
+    [SerializeField] 
+    GameObject projectile6;
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            projectiles.SetActive(true);
+            projectile1.SetActive(true);
+            projectile2.SetActive(true);
+            projectile3.SetActive(true);
+            projectile4.SetActive(true);
+            projectile5.SetActive(true);
+            projectile6.SetActive(true);
         }
     }
 }
